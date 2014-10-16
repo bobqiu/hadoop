c825c66cf04e25e2eb8030f5168e69ee18d551f3
bobqiu <xiaoqiu369@gmail.com>
2014-10-16 21:06:44 +0800

    init add for importing data to hbase and using MR to insert data.

---
+++ src/main/java/com/qiu/hbase/PrintUserCount.java
@@ -0,0 +1,57 @@
+
+package com.qiu.hbase;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class PrintUserCount {
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws ZooKeeperConnectionException
+     * @throws MasterNotRunningException
+     */
+    public static void main(String[] args) throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
+        // TODO Auto-generated method stub
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.quorum", "DamHadoop1,DamHadoop2,DamHadoop3");
+        HTable hTable = new HTable(conf, "import_tests");
+        //HTable hTable = new HTable(conf, "summary_user");
+        Scan scan = new Scan();
+        ResultScanner rs = hTable.getScanner(scan);
+        for (Result r : rs) {
+            for (Cell cell : r.rawCells()) {
+                System.out.println("行键："+Bytes.toInt(CellUtil.cloneRow(cell)));
+                System.out.println("簇键："+new String(CellUtil.cloneFamily(cell)));
+            }
+        }
+        /*Scan scan = new Scan();
+        ResultScanner scanner = hTable.getScanner(scan);
+        Result r;
+        while (((r = scanner.next()) != null)) {
+            //ImmutableBytesWritable b = r.getBytes();
+            byte[] key = r.getRow();
+            int userId = Bytes.toInt(key);
+            byte[] totalValue = r.getValue(Bytes.toBytes("details"), Bytes.toBytes("total"));
+            int count = Bytes.toInt(totalValue);
+
+            System.out.println("key: " + userId+ ",  count: " + count);
+        }
+        scanner.close();
+        hTable.close();*/
+    }
+
+}
---
+++ src/main/java/com/qiu/mptest/TestHadoopCodec.java
@@ -0,0 +1,76 @@
+
+package com.qiu.mptest;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.util.ReflectionUtils;
+
+public class TestHadoopCodec {
+
+    /**
+     * @param args
+     * @throws Exception 
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException, Exception {
+        // TODO Auto-generated method stub
+        String inputFile = "";
+        String outPutDir = "hdfs://DamHadoop1:9000/user/hadoop/codec/";
+        //hadoop文件的配置信息
+        Configuration conf=new Configuration();
+        conf.set("hadoop.job.ugi", "hadoop,hadoop");
+        
+      //测试各种压缩格式的效率 
+        //gzip 
+        long gzipTime = zipFile(conf, inputFile, outPutDir, "org.apache.hadoop.io.compress.GzipCodec", "gz"); 
+        //bzip2 
+        long bzip2Time = zipFile(conf, inputFile, outPutDir, "org.apache.hadoop.io.compress.BZip2Codec", "bz2"); 
+        //deflate 
+        long deflateTime = zipFile(conf, inputFile, outPutDir, "org.apache.hadoop.io.compress.DefaultCodec", "deflate"); 
+         
+        System.out.println("被压缩的文件名为： "+inputFile); 
+        System.out.println("使用gzip压缩，时间为： "+gzipTime+"毫秒!"); 
+        System.out.println("使用bzip2压缩，时间为： "+bzip2Time+"毫秒!"); 
+        System.out.println("使用deflate压缩，时间为： "+deflateTime+"毫秒!"); 
+    }
+
+    static long zipFile(Configuration conf, String inputFile, String outputFolder, String codecClassName,
+            String suffixName) throws IOException, Exception {
+        long startTime=System.currentTimeMillis();
+        //创建本地文件输入流
+        InputStream is=new BufferedInputStream(new FileInputStream(inputFile));
+        //获取文件名
+        String fileName=inputFile.substring(0,inputFile.indexOf("."));
+        //构建输出文件名
+        String outFile=outputFolder+fileName+"."+suffixName;
+        
+        //构建hadoop文件系统
+        FileSystem fs=FileSystem.get(URI.create(outFile),conf);
+        
+        //创建一个编码解码器，通过反射机制根据传入的来动态生成实例
+        CompressionCodec codec=(CompressionCodec) ReflectionUtils.newInstance(Class.forName(codecClassName), conf);
+        
+        //创建一个指向hdfs目录的压缩文件输出流
+        OutputStream os=codec.createOutputStream(fs.create(new Path(outFile)));
+        try {
+            IOUtils.copyBytes(is, os, conf);
+        } catch (Exception e) {
+            is.close();
+            os.close();
+            // TODO: handle exception
+        }
+        long endTime=System.currentTimeMillis();
+        
+        return endTime-startTime;
+    }
+}
